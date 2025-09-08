e5c4ef7e7c53cd6293a1e0bb937281d032fa05a3
raulpesilva <adraulpesilva@gmail.com>
2025-09-07 23:18:30 -0300

    chore: initial

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useReState } from '@raulpesilva/re-state';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+  const [count, setCount] = useReState('count', 0);
+
+  return (
+    <>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>Remote App</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { federation } from '@module-federation/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { dependencies } from './package.json';
+
+export default defineConfig(() => {
+  return {
+    server: { fs: { allow: ['.', '../shared'] } },
+    build: { target: 'chrome89' },
+    plugins: [
+      federation({
+        filename: 'remoteEntry.js',
+        name: 'remote',
+        exposes: { './remote-app': './src/App.tsx' },
+        remotes: {},
+        shared: {
+          react: {
+            requiredVersion: dependencies.react,
+            singleton: true,
+          },
+          '@raulpesilva/re-state': {
+            requiredVersion: dependencies['@raulpesilva/re-state'],
+            singleton: true,
+          },
+        },
+      }),
+      react(),
+    ],
+  };
+});
