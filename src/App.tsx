a3270bc1acd9cc6dd47c5cb172c6afbc46003ea9
raulpesilva <adraulpesilva@gmail.com>
2025-09-21 22:55:21 -0300

    feat: add @tailwindcss/oxide dependency and update App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BannerIcon from './assets/banner-un-draw.svg';
+import './index.css';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
         <Route
           path='/'
           element={
-            <section className='w-full h-full flex flex-col items-center justify-center px-4'>
+            <section className='w-full h-full flex flex-col items-center justify-center px-4 gap-12'>
+              <img src={BannerIcon} alt='Banner' className='w-1/2 max-w-[448px]' />
               <img src={BannerIcon} alt='Banner' className='w-1/2 max-w-[448px]' />
             </section>
           }
