48ec67102b65df628513a6e2b69a10078ca0d9e7
raulpesilva <adraulpesilva@gmail.com>
2025-09-25 22:25:53 -0300

    feat: simplify getToken function by removing unnecessary invocation

--- src/states/token/index.ts
+++ src/states/token/index.ts
@@ -4,4 +4,4 @@ export const TOKEN_KEY = 'token';
 export const initialTokenValue = null as string | null;
 
 export const dispatchToken = createReStateDispatch<string | null>(TOKEN_KEY);
-export const getToken = () => createGetReState<string | null>(TOKEN_KEY)();
+export const getToken = createGetReState<string | null>(TOKEN_KEY);
